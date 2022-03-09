ee7f024170143dd837c4bc424ea53a8c8c14ef6a
onesg <gabriel.seno@fatec.sp.gov.br>
2022-03-08 22:08:47 -0300

    Primeiro commit

---
+++ src/test/java/br/com/projetoagendafatec/agendafatecapi/AgendafatecApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetoagendafatec.agendafatecapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendafatecApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

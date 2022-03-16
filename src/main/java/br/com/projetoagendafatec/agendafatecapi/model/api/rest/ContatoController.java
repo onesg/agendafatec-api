707598264ee362ad35a486204eb5ff29a68181cb
onesg <gabriel.seno@fatec.sp.gov.br>
2022-03-15 22:26:47 -0300

    Adicionado notação @RequestMapping

--- src/main/java/br/com/projetoagendafatec/agendafatecapi/model/api/rest/ContatoController.java
+++ src/main/java/br/com/projetoagendafatec/agendafatecapi/model/api/rest/ContatoController.java
@@ -1,8 +1,10 @@
 // obs: irá funcionar como uma servlet, porém usando o REST
 package br.com.projetoagendafatec.agendafatecapi.model.api.rest;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/contatos") //
 public class ContatoController {
 }

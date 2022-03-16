f40dabecc996ef3cfdce2239f72a859b36e2c6f5
onesg <gabriel.seno@fatec.sp.gov.br>
2022-03-15 22:21:22 -0300

    Adicionado extends a ContatoRepository

--- src/main/java/br/com/projetoagendafatec/agendafatecapi/model/repository/ContatoRepository.java
+++ src/main/java/br/com/projetoagendafatec/agendafatecapi/model/repository/ContatoRepository.java
@@ -1,4 +1,8 @@
 package br.com.projetoagendafatec.agendafatecapi.model.repository;
 
-public interface ContatoRepository {
+import br.com.projetoagendafatec.agendafatecapi.model.entity.Contato;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContatoRepository extends JpaRepository<Contato, Integer> {
+    //  classe contato tem um reposotório Jpa, podendo usar os métodos do Jpa.
 }

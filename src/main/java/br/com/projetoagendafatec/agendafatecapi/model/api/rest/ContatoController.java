814619bde17441fd96614228fc4dada7eb86a3e1
onesg <gabriel.seno@fatec.sp.gov.br>
2022-03-15 22:22:45 -0300

    Criação da ContatoController

---
+++ src/main/java/br/com/projetoagendafatec/agendafatecapi/model/api/rest/ContatoController.java
@@ -0,0 +1,8 @@
+// obs: irá funcionar como uma servlet, porém usando o REST
+package br.com.projetoagendafatec.agendafatecapi.model.api.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContatoController {
+}

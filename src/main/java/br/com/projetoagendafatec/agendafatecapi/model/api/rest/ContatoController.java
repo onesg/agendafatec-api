380a9648baad165811664b1d9c5a14477b85c27b
onesg <gabriel.seno@fatec.sp.gov.br>
2022-03-19 15:25:32 -0300

    Implementado os métodos salvar, listar e deletar

--- src/main/java/br/com/projetoagendafatec/agendafatecapi/model/api/rest/ContatoController.java
+++ src/main/java/br/com/projetoagendafatec/agendafatecapi/model/api/rest/ContatoController.java
@@ -1,10 +1,40 @@
 // obs: irá funcionar como uma servlet, porém usando o REST
 package br.com.projetoagendafatec.agendafatecapi.model.api.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import br.com.projetoagendafatec.agendafatecapi.model.repository.ContatoRepository;
+import br.com.projetoagendafatec.agendafatecapi.model.entity.Contato;
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/contatos") //
+@RequestMapping("/api/contatos")
+@RequiredArgsConstructor
+@CrossOrigin("*")
+
 public class ContatoController {
+
+    private final ContatoRepository repository;
+
+    /* método save */
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Contato save(@RequestBody Contato contato){
+        return repository.save(contato);
+    }
+
+    /* método listar */
+    @GetMapping
+    public List<Contato> list(){
+        return repository.findAll();
+    }
+
+    /* metodo delete */
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Integer id){
+        repository.deleteById(id);
+    }
+
 }

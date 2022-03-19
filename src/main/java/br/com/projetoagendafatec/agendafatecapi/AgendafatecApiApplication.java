274be0cfa61a44e0b0ee1ac81d2dc2be40225696
onesg <gabriel.seno@fatec.sp.gov.br>
2022-03-19 15:23:18 -0300

    Adicionado importações e método para teste

--- src/main/java/br/com/projetoagendafatec/agendafatecapi/AgendafatecApiApplication.java
+++ src/main/java/br/com/projetoagendafatec/agendafatecapi/AgendafatecApiApplication.java
@@ -1,10 +1,31 @@
 package br.com.projetoagendafatec.agendafatecapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import br.com.projetoagendafatec.agendafatecapi.model.repository.ContatoRepository;
+import br.com.projetoagendafatec.agendafatecapi.model.entity.Contato;
+
 
 @SpringBootApplication
 public class AgendafatecApiApplication {
+
+	/*
+	@Bean
+	public CommandLineRunner run(@Autowired ContatoRepository repository) {
+		return args -> {
+			Contato contato = new Contato();
+			contato.setNome("Gabriel");
+			contato.setEmail("test@test.com");
+			contato.setFavorito(false);
+			repository.save(contato);
+
+		};
+
+	}
+	*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(AgendafatecApiApplication.class, args);
